3d241d2c8552891436f583cfdbb123e97398caa9
zhangce <38835461+zhang874010534@users.noreply.github.com>
2026-06-09 23:38:16 +0800

    项目目录创建、inject使用

--- LLMOps平台/LLMOps-api/main.py
+++ LLMOps平台/LLMOps-api/main.py
@@ -4,3 +4,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+from injector import Injector, inject
+
+class A:
+    name: str = "LLMOps"
+
+@inject
+class B:
+    def __init__(self, a: A):
+        self.a = a
+    def print(self):
+        print(f"{self.a.name}")
